53fce4d8538ca40fc804918d232ab5cb95564e92
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2020-02-18 15:08:40 +0300

    added schedule task to end session page

--- src/main/resources/static/js/plugins/task/end-sessions.js
+++ src/main/resources/static/js/plugins/task/end-sessions.js
@@ -1,17 +1,17 @@
 /**
-* Task is end-sessions task
-* this task terminates open user sessions on clients
-* Tuncay ÇOLAK
-* tuncay.colak@tubitak.gov.tr
-* 
-* http://www.liderahenk.org/
-* 
-*/
+ * end-sessions
+ * this task terminates open user sessions on clients
+ * Tuncay ÇOLAK
+ * tuncay.colak@tubitak.gov.tr
+ * 
+ * http://www.liderahenk.org/
+ * 
+ */
 
 if (ref) {
 	connection.deleteHandler(ref);
 }
-
+scheduledParam = null;
 var ref=connection.addHandler(endSessionsListener, null, 'message', null, null,  null);
 $("#entrySize").html(selectedEntries.length);		
 var dnlist=[]
@@ -19,22 +19,53 @@ for (var i = 0; i < selectedEntries.length; i++) {
 	dnlist.push(selectedEntries[i].distinguishedName);
 }
 
-selectedPluginTask.dnList=dnlist;
-selectedPluginTask.parameterMap={};
-selectedPluginTask.entryList=selectedEntries;
-selectedPluginTask.dnType="AHENK";
-var params = JSON.stringify(selectedPluginTask);
+function sendTaskEndSession() {
+	selectedPluginTask.dnList=dnlist;
+	selectedPluginTask.parameterMap={};
+	selectedPluginTask.entryList=selectedEntries;
+	selectedPluginTask.dnType="AHENK";
+	selectedPluginTask.cronExpression = scheduledParam;
+	var params = JSON.stringify(selectedPluginTask);
+
+	var message = "Görev başarı ile gönderildi.. Lütfen bekleyiniz...";
+	if (scheduledParam != null) {
+		message = "Zamanlanmış görev başarı ile gönderildi. Zamanlanmış görev parametreleri:  "+ scheduledParam;
+	}
+	$.ajax({
+		type: "POST",
+		url: "/lider/task/execute",
+		headers: {
+			'Content-Type':'application/json',
+		}, 
+		data: params,
+		contentType: "application/json",
+		dataType: "json",
+		converters: {
+			'text json': true
+		}, 
+		success: function(result) {
+			var res = jQuery.parseJSON(result);
+			if(res.status=="OK"){
+				$("#plugin-result").html(message.bold());
+			}   	
+			/* $('#closePage').click(); */
+		},
+		error: function(result) {
+			$.notify(result, "error");
+		}
+	});
+}
 
 function endSessionsListener(msg) {
-    var to = msg.getAttribute('to');
-    var from = msg.getAttribute('from');
-    var type = msg.getAttribute('type');
-    var elems = msg.getElementsByTagName('body');
-    
-    if (type == "chat" && elems.length > 0) {
-    	var body = elems[0];
-    	var data=Strophe.xmlunescape(Strophe.getText(body));
-    	var xmppResponse=JSON.parse(data);
+	var to = msg.getAttribute('to');
+	var from = msg.getAttribute('from');
+	var type = msg.getAttribute('type');
+	var elems = msg.getElementsByTagName('body');
+
+	if (type == "chat" && elems.length > 0) {
+		var body = elems[0];
+		var data=Strophe.xmlunescape(Strophe.getText(body));
+		var xmppResponse=JSON.parse(data);
 		if(xmppResponse.commandClsId == "MANAGE"){
 			if (xmppResponse.result.responseCode == "TASK_PROCESSED" || xmppResponse.result.responseCode == "TASK_ERROR") {
 				if (xmppResponse.result.responseCode == "TASK_PROCESSED") {
@@ -46,44 +77,33 @@ function endSessionsListener(msg) {
 				}
 			}
 		}						 
-    }
-    // we must return true to keep the handler alive. returning false would remove it after it finishes.
-    return true;
+	}
+	// we must return true to keep the handler alive. returning false would remove it after it finishes.
+	return true;
 }
 
 $('#sendTask-'+ selectedPluginTask.page).click(function(e){
-	
-	$.ajax({
-	    type: "POST",
-	    url: "/lider/task/execute",
-	    headers: {
-	        'Content-Type':'application/json',
-	    }, 
-	    data: params,
-	    contentType: "application/json",
-	    dataType: "json",
-	    converters: {
-	      'text json': true
-	    }, 
-	    success: function(result) {
-	  	var res = jQuery.parseJSON(result);
-	  	if(res.status=="OK"){
-	  		$("#plugin-result").html("Görev başarı ile gönderildi.. Lütfen bekleyiniz...");
-	  	}   	
-	      /* $('#closePage').click(); */
-	    },
-	    error: function(result) {
-	  	  $.notify(result, "error");
-	    }
+	var content = "Görev Gönderilecek, emin misiniz?";
+	if (scheduledParam != null) {
+		content = "Zamanlanmış görev gönderilecek, emin misiniz?";
+	}
+	$.confirm({
+		title: 'Uyarı!',
+		content: content,
+		theme: 'light',
+		buttons: {
+			Evet: function () {
+				sendTaskEndSession();
+				scheduledParam = null;
+			},
+			Hayır: function () {
+			}
+		}
 	});
 });
 
-//scheduled task to be added 
-$('#sendTaskCron-'+ selectedPluginTask.page).click(function(e){
-	alert("Zamanlı Çalıştır")
-});
-
 $('#closePage-'+ selectedPluginTask.page).click(function(e){
-	connection.deleteHandler(ref);	
+	connection.deleteHandler(ref);
+	scheduledParam = null;
 });
 

c6d637147f2f3a9a82223c35635629819f484b13
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2020-03-09 16:39:07 +0300

    chart.js has been added to main

--- src/main/resources/static/js/plugins/task/resource-usage.js
+++ src/main/resources/static/js/plugins/task/resource-usage.js
@@ -10,68 +10,75 @@
 if (refResUsage) {
 	connection.deleteHandler(refResUsage);
 }
+var usageMemory = 50;
+var freeMemory = 50;
+var usageDisk = 50;
+var freeDisk = 50;
+var cpu_actual1 = 50;
+var cpu_advertised1 = 50;
+var systemChart1;
+var systemChart2;
+var systemChart3;
+
 scheduledParam=null;
 var refResUsage=connection.addHandler(resourceUsageListener, null, 'message', null, null,  null); 
 
 var pluginTask_ResourceUsage=null
 
-
-
 for (var n = 0; n < pluginTaskList.length; n++) {
-		var pluginTask=pluginTaskList[n];
-		if(pluginTask.page == 'resource-usage')
-		{
-			pluginTask_ResourceUsage=pluginTask;
-		}
+	var pluginTask=pluginTaskList[n];
+	if(pluginTask.page == 'resource-usage')
+	{
+		pluginTask_ResourceUsage=pluginTask;
+	}
 }
 
+createCharts();
 
 function getResourceUsage(){
 	var dnlist=[]
 	for (var i = 0; i < selectedEntries.length; i++) {
 		dnlist.push(selectedEntries[i].distinguishedName);
-		alert(selectedEntries[i].distinguishedName)
 	}
-	
+
 	if(pluginTask_ResourceUsage){
-		
 		pluginTask_ResourceUsage.commandId='RESOURCE_INFO_FETCHER'
-		pluginTask_ResourceUsage.dnList=dnlist;
+			pluginTask_ResourceUsage.dnList=dnlist;
 		pluginTask_ResourceUsage.parameterMap={};
 		pluginTask_ResourceUsage.entryList=selectedEntries;
 		pluginTask_ResourceUsage.dnType="AHENK";
 		pluginTask_ResourceUsage.cronExpression = scheduledParam;
-		
+
 		var params = JSON.stringify(pluginTask_ResourceUsage);
-		
+
 		var message = "Görev başarı ile gönderildi.. Lütfen bekleyiniz...";
 		if (scheduledParam != null) {
-				message = "Zamanlanmış görev başarı ile gönderildi. Zamanlanmış görev parametreleri: "+ scheduledParam;
+			message = "Zamanlanmış görev başarı ile gönderildi. Zamanlanmış görev parametreleri: "+ scheduledParam;
 		}
-		
-			$.ajax({
-				type: "POST",
-				url: "/lider/task/execute",
-				headers: {
-					'Content-Type':'application/json',
-				}, 
-				data: params,
-				contentType: "application/json",
-				dataType: "json",
-				converters: {
-					'text json': true
-				}, 
-				success: function(result) {
-					var res = jQuery.parseJSON(result);
-					if(res.status=="OK"){
-						$("#plugin-result-resource-usage").html(message.bold());
-					}   	
-					/* $('#closePage').click(); */
-				},
-				error: function(result) {
-					$.notify(result, "error");
-				}
-			});
+
+		$.ajax({
+			type: "POST",
+			url: "/lider/task/execute",
+			headers: {
+				'Content-Type':'application/json',
+			}, 
+			data: params,
+			contentType: "application/json",
+			dataType: "json",
+			converters: {
+				'text json': true
+			}, 
+			success: function(result) {
+				var res = jQuery.parseJSON(result);
+				if(res.status=="OK"){
+					$("#plugin-result-resource-usage").html(message.bold());
+				}   	
+				/* $('#closePage').click(); */
+			},
+			error: function(result) {
+				$.notify(result, "error");
+			}
+		});
 	}
 }
 
@@ -80,7 +87,7 @@ $('#sendTask-resource-usage').click(function(e){
 		$.notify("Lütfen İstemci Seçiniz", "error");
 		return;
 	}
-	
+
 	var content = "Görev Gönderilecek, emin misiniz?";
 	if (scheduledParam != null) {
 		content = "Zamanlanmış görev gönderilecek, emin misiniz?";
@@ -105,7 +112,7 @@ function resourceUsageListener(msg) {
 	var from = msg.getAttribute('from');
 	var type = msg.getAttribute('type');
 	var elems = msg.getElementsByTagName('body');
-	
+
 	if (type == "chat" && elems.length > 0) {
 		var body = elems[0];
 		var data=Strophe.xmlunescape(Strophe.getText(body));
@@ -138,6 +145,22 @@ function resourceUsageListener(msg) {
 					$("#usage_disk").html(arrg["Usage Disc"]+" MB");
 					$("#plugin-result-resource-usage").html("");
 					$.notify(xmppResponse.result.responseMessage, "success");
+					
+					usageMemory = (arrg["Usage"]/arrg["Total Memory"]*100).toFixed(2);
+					freeMemory = (100 - usageMemory).toFixed(2);
+					usageDisk = (arrg["Usage Disc"]/arrg["Total Disc"]*100).toFixed(2);
+					freeDisk = (100 - usageDisk).toFixed(2);
+//					cpu_actual = arrg["CPU Actual Hz"];
+//					cpu_advertised = arrg["CPU Advertised Hz"];
+					
+					cpu_actual1 = 15;
+					cpu_advertised1 = 85;
+					
+					systemChart1.destroy();
+					systemChart2.destroy();
+					systemChart3.destroy();
+					
+					createCharts();
 
 				} else {
 					$("#plugin-result").html(("HATA: "+ xmppResponse.result.responseMessage).fontcolor("red"));
@@ -150,7 +173,69 @@ function resourceUsageListener(msg) {
 	return true;
 }
 
-//$('#closePage-'+ selectedPluginTask.page).click(function(e){
-//	connection.deleteHandler(ref);
-//	scheduledParam=null;
-//});
+
+function createCharts() {
+	
+	console.log(usageMemory)
+	console.log(freeMemory)
+	console.log(usageDisk)
+	console.log(freeDisk)
+	
+	var memoryChart = document.getElementById("memoryInfoChart").getContext('2d');
+	systemChart1 = new Chart(memoryChart, {
+		type: 'doughnut',
+		data: {
+			labels: ["Kullanılan (%)", "Boş (%)"],
+			datasets: [{
+				data: [usageMemory, freeMemory],
+				backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+				hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+			}]
+		},
+		options: {
+			responsive: true,
+			legend: {
+				display: false
+			}
+		}
+	});
+
+	var diskChart = document.getElementById("diskInfoChart").getContext('2d');
+	systemChart2 = new Chart(diskChart, {
+		type: 'doughnut',
+		data: {
+			labels: ["Kullanılan(%)", "Boş(%)"],
+			datasets: [{
+				data: [usageDisk, freeDisk],
+				backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+				hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+			}]
+		},
+		options: {
+			responsive: true,
+			legend: {
+				display: false
+			}
+		}
+	});
+
+	var processorChart = document.getElementById("processorInfoChart").getContext('2d');
+	systemChart3 = new Chart(processorChart, {
+		type: 'doughnut',
+		data: {
+			labels: ["Gerçek Frekans(GHz)", "Beklenen Frekans(GHz)"],
+			datasets: [{
+				data: [cpu_actual1, cpu_advertised1],
+				backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+				hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+			}]
+		},
+		options: {
+			responsive: true,
+			legend: {
+				display: false
+			}
+		}
+	});
+
+}

1e77fb71384e499d28348418c4219686f6919aac
Hasan Kara <h.kara27@gmail.com>
2020-06-29 11:19:03 +0300

    application.properties will be retrieved from external file. External file location is /etc/lider/lider.properties

--- src/main/java/tr/org/lider/Lider2Application.java
+++ src/main/java/tr/org/lider/Lider2Application.java
@@ -1,6 +1,7 @@
 package tr.org.lider;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Properties;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -10,11 +11,23 @@ public class Lider2Application extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Lider2Application.class);
+		return application.sources(Lider2Application.class)
+				.sources(Lider2Application.class)
+				.properties(getProperties());
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lider2Application.class, args);
+		new SpringApplicationBuilder(Lider2Application.class)
+		.sources(Lider2Application.class)
+		.properties(getProperties())
+		.run(args);
+	}
+
+
+	static Properties getProperties() {
+		Properties props = new Properties();
+		props.put("spring.config.location","file:/etc/lider/lider.properties");
+		return props;
 	}
 
 }

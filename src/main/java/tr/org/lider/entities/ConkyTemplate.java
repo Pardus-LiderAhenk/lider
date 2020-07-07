c7bbf39d80c3881ba0ef5575bea2005086d4a651
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2020-07-07 20:22:47 +0300

    add timezone to entities

--- src/main/java/tr/org/lider/entities/ConkyTemplate.java
+++ src/main/java/tr/org/lider/entities/ConkyTemplate.java
@@ -48,12 +48,12 @@ public class ConkyTemplate implements Serializable {
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "CREATE_DATE", nullable = false, updatable = false)
 	@CreationTimestamp
-	@JsonFormat(pattern="dd/MM/yyyy HH:mm:ss")
+	@JsonFormat(pattern="dd/MM/yyyy HH:mm:ss", timezone="Europe/Istanbul")
 	private Date createDate;
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "MODIFY_DATE")
-	@JsonFormat(pattern="dd/MM/yyyy HH:mm:ss")
+	@JsonFormat(pattern="dd/MM/yyyy HH:mm:ss", timezone="Europe/Istanbul")
 	private Date modifyDate;
 
 	public ConkyTemplate() {
